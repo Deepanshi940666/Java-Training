053f64cccd87a31d5fbc76d7f401b42bdd1b6949
Deepanshi Gupta <deepanshi102938@gmail.com>
2025-11-11 23:12:21 +0530

    Introduction and Basic concept

---
+++ lec1/datatype.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class datatype{
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+
+        System.out.print("Enter int: ");
+        int a=sc.nextInt();
+
+        System.out.print("Enter Double: ");
+        double b=sc.nextDouble();
+
+        System.out.print("Enter String: ");
+        String c=sc.next();
+        sc.nextLine();
+
+        System.out.print("Enter Line: ");
+        String d=sc.nextLine();
+
+        System.out.println("Integer: "+a);
+        System.out.println("Double: "+b);
+        System.out.println("String: "+c);
+        System.out.println("Line: "+d);
+    }
+}
--- lec1/intro.java
+++ lec1/intro.java
@@ -1,4 +1,4 @@
-class Main {
+class intro {
     public static void main(String[] args) {
         System.out.println("Hello world");
         byte a=127;
