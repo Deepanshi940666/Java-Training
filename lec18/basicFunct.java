bdfee2dfc599d2e7d999bbae651d185e7f8c62e1
Deepanshi Gupta <deepanshi102938@gmail.com>
2025-12-12 13:38:35 +0530

    lec18

---
+++ lec18/basicFunct.java
@@ -0,0 +1,19 @@
+package lec18;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class basicFunct {
+    public static void main(String[] args) {
+        ArrayList<Integer> list=new ArrayList<>();
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        list.add(50);
+        list.add(60);
+        List<Integer>res=list.stream().filter(n->n>30).toList();
+        List<Integer>mapres=list.stream().map(n->n=n*3).toList();
+        int data=list.stream().reduce(0,(a,b)->a+b);
+        System.out.println(data);
+    }
+}
